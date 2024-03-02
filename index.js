e1a82b907abe94a1d1338fd9aec9c93e8e5c70e8
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2024-03-02 23:04:56 +0200

    Refactor variable names and function names in index.js

--- index.js
+++ index.js
@@ -3,43 +3,43 @@ let homeScore = document.getElementById("home-score-text");
 // This is to get the id for the guest score
 let guestScore = document.getElementById("guest-score-text");
 // This is to set the home score to 0
-let homeone = 0;
+let homeOne = 0;
 // This is to set the guest score to 0
-let vartwo = 0;
+let varTwo = 0;
 // This is to add 1 to the home score when the 1 point button is pressed
 function one() {
-  homeone += 1;
-  homeScore.textContent = homeone;
+  homeOne += 1;
+  homeScore.textContent = homeOne;
 }
 // This is to add 2 to the home score when the 2 point button is pressed
 function two() {
-  homeone += 2;
-  homeScore.textContent = homeone;
+  homeOne += 2;
+  homeScore.textContent = homeOne;
 }
 // This is to add 3 to the home score when the 3 point button is pressed
 function three() {
-  homeone += 3;
-  homeScore.textContent = homeone;
+  homeOne += 3;
+  homeScore.textContent = homeOne;
 }
 // This is to add 1 to the guest score when the 1 point button is pressed
-function guestone() {
-  vartwo += 1;
-  guestScore.textContent = vartwo;
+function guestOne() {
+  varTwo += 1;
+  guestScore.textContent = varTwo;
 }
 // This is to add 2 to the guest score when the 2 point button is pressed
-function guesttwo() {
-  vartwo += 2;
-  guestScore.textContent = vartwo;
+function guestTwo() {
+  varTwo += 2;
+  guestScore.textContent = varTwo;
 }
 // This is to add 3 to the guest score when the 3 point button is pressed
-function guestthree() {
+function guestThree() {
   vartwo += 3;
-  guestScore.textContent = vartwo;
+  guestScore.textContent = varTwo;
 }
 // This is to reset the scores to 0
-function newgame() {
-  vartwo = 0;
-  homeone = 0;
-  guestScore.textContent = vartwo;
-  homeScore.textContent = homeone;
+function newGame() {
+  varTwo = 0;
+  homeOne = 0;
+  guestScore.textContent = varTwo;
+  homeScore.textContent = homeOne;
 }
